8f7ea4a2c780041ee77233086571986c57788718
Romi Gupta <romi35532@gmail.com>
2024-03-04 20:27:56 +0530

    day4 update

---
+++ DAY4/QUERY4.sql
@@ -0,0 +1,22 @@
+
+
+drop table if exists Q4_data;
+create table Q4_data
+(
+	id			int,
+	name		varchar(20),
+	location	varchar(20)
+);
+insert into Q4_data values(1,null,null);
+insert into Q4_data values(2,'David',null);
+insert into Q4_data values(3,null,'London');
+insert into Q4_data values(4,null,null);
+insert into Q4_data values(5,'David',null);
+
+select * from Q4_data;
+
+-- Question : Derived Excepted Ouput 
+
+SELECT MIN(id),MIN(name),MIN(location) FROM Q4_data;
+-- OR ALSO WE CAN GET AS BELOW 
+SELECT MAX(id),MAX(name),MIN(location) FROM Q4_data;
