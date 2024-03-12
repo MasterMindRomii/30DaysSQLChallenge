d6e36fbb8859faf6abd57fed27af2a89762249cf
Romi Gupta <romi35532@gmail.com>
2024-03-13 00:57:32 +0530

    Day-4 Update

--- DAY4/QUERY4.sql
+++ DAY4/QUERY4.sql
@@ -13,7 +13,6 @@ insert into Q4_data values(3,null,'London');
 insert into Q4_data values(4,null,null);
 insert into Q4_data values(5,'David',null);
 
-select * from Q4_data;
 
 -- Question : Derived Excepted Ouput 
 
